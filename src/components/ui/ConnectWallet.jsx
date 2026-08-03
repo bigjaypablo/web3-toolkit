b362f4d3d83f3472a3325044bdf5850978de58ec
Big Jay <johnsoncarter50@gmail.com>
2026-08-03 22:44:10 +0100

    Use Reown AppKit as the only wallet connection modal

--- src/components/ui/ConnectWallet.jsx
+++ src/components/ui/ConnectWallet.jsx
@@ -1,35 +1,5 @@
-import { useAccount } from "wagmi";
-import { useAppKit } from "@reown/appkit/react";
+import { WalletManager } from "../web3/WalletManager";
 
-function ConnectWallet() {
-  const { address, isConnected } = useAccount();
-  const { open } = useAppKit();
-
-  function shortenAddress(address) {
-    return `${address.slice(0, 6)}...${address.slice(-4)}`;
-  }
-
-  if (isConnected && address) {
-    return (
-      <button
-        type="button"
-        className="connected-wallet-button"
-        onClick={() => open()}
-      >
-        {shortenAddress(address)}
-      </button>
-    );
-  }
-
-  return (
-    <button
-      type="button"
-      className="connect-wallet-button"
-      onClick={() => open()}
-    >
-      Connect Wallet
-    </button>
-  );
+export default function ConnectWallet() {
+  return <WalletManager />;
 }
-
-export default ConnectWallet;
